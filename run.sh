ec9881c25101b1420affccaa538acd08a9d73079
Hanna Reitz <hanna@xanclic.moe>
2017-07-18 22:55:45 +0200

    Add HTIF serial driver
    + Add HTIF serial driver ("simple non-standard console device")
    + Print something nice
    
    Signed-off-by: Hanna Reitz <hanna@xanclic.moe>

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-../riscv-qemu/build/riscv64-softmmu/qemu-system-riscv64 -kernel kernel -monitor stdio
+../riscv-qemu/build/riscv64-softmmu/qemu-system-riscv64 -kernel kernel -serial stdio
