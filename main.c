ef952a782de46b9811d8734c0afe8d7ad5949175
Hanna Reitz <hanna@xanclic.moe>
2017-07-19 04:40:23 +0200

    Some refactoring
    * Put some stuff into own functions
    * Let putchar() print \r before \n automatically
    * Let puts() print a newline
    
    Signed-off-by: Hanna Reitz <hanna@xanclic.moe>

--- main.c
+++ main.c
@@ -10,15 +10,30 @@ struct htif_register {
 volatile struct htif_register __attribute__((section("htif"))) tohost, fromhost;
 
 
-void putchar(uint8_t c)
+void htif_write_to_host(uint8_t device, uint8_t function, uint64_t payload)
 {
-    tohost.lo = c;
-    /* device 1 (serial), function 1 (put string) */
-    tohost.hi = (1 << 24) | (1 << 16);
+    tohost.lo = payload & 0xffffffffu;
+    tohost.hi = ((uint32_t)device << 24) | ((uint32_t)function << 16) | ((payload >> 32) & 0xffff);
+}
 
-    /* acknowledge result */
+void htif_clear_from_host(void)
+{
     fromhost.lo = 0;
     fromhost.hi = 0;
+}
+
+
+void putchar(uint8_t c)
+{
+    if (c == '\n') {
+        putchar('\r');
+    }
+
+    /* device 1 (console), function 1 (put string) */
+    htif_write_to_host(1, 1, c);
+
+    /* acknowledge result */
+    htif_clear_from_host();
 }
 
 
@@ -27,10 +42,11 @@ void puts(const char *str)
     while (*str) {
         putchar(*(str++));
     }
+    putchar('\n');
 }
 
 
 void main(void)
 {
-    puts("Hello, RISC-V world!\r\n");
+    puts("Hello, RISC-V world!");
 }
