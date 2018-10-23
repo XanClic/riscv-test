a9e92394a89880aabe18475249a1f0dcce642d84
Hanna Reitz <hanna@xanclic.moe>
2018-10-23 14:06:31 +0100

    stddef: Add some helpers
    Strictly speaking these do not belong here, but why not.
    
    Signed-off-by: Hanna Reitz <hanna@xanclic.moe>

--- include/stddef.h
+++ include/stddef.h
@@ -7,6 +7,15 @@
 
 typedef unsigned long size_t;
 
+#ifndef offsetof
 #define offsetof(t, m) ((size_t)(&(((t *)0)->m)))
+#endif
+
+#ifndef ARRAY_SIZE
+#define ARRAY_SIZE(a) (sizeof(a) / sizeof((a)[0]))
+#endif
+
+#define MIN(x, y) ((x) < (y) ? (x) : (y))
+#define MAX(x, y) ((x) > (y) ? (x) : (y))
 
 #endif
