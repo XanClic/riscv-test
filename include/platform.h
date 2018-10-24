8183e9e7ab24106282fa5c54aac168a4606c9eaa
Hanna Reitz <hanna@xanclic.moe>
2018-10-24 16:04:02 +0100

    platform: Add audio methods
    Signed-off-by: Hanna Reitz <hanna@xanclic.moe>

--- include/platform.h
+++ include/platform.h
@@ -26,6 +26,12 @@ typedef struct PlatformFuncs {
     bool (*get_keyboard_event)(int *key, bool *up);
     bool (*get_mouse_event)(int *dx, int *dy, bool *has_button, int *button,
                             bool *button_up);
+
+    // completed() must be quick to return and may not call any audio
+    // function
+    bool (*queue_audio_track)(const int16_t *buffer, size_t samples,
+                              void (*completed)(void));
+    void (*handle_audio)(void);
 } PlatformFuncs;
 
 
